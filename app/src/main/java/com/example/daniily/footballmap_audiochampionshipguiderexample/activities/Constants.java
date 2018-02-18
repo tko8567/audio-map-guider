8bc9a67c6043bfddadbb4fc0aeec77d29f604a5c
DPolyakov <dimasona707@gmail.com>
2018-02-18 15:01:14 +0200

    Add place model

---
+++ app/src/main/java/com/example/daniily/footballmap_audiochampionshipguiderexample/activities/Constants.java
@@ -0,0 +1,9 @@
+package com.example.daniily.footballmap_audiochampionshipguiderexample.activities;
+
+/**
+ * Created by Dmitrii Polyakov on KENIGHACK 2018.
+ */
+
+public class Constants {
+    public enum Localization {RUS, ENG}
+}
---
+++ app/src/main/java/com/example/daniily/footballmap_audiochampionshipguiderexample/activities/networking/models/Place.java
@@ -0,0 +1,55 @@
+package com.example.daniily.footballmap_audiochampionshipguiderexample.activities.networking.models;
+
+import com.example.daniily.footballmap_audiochampionshipguiderexample.activities.Constants;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dmitrii Polyakov on KENIGHACK 2018.
+ */
+
+public class Place {
+
+    ArrayList<PlaceLocate> locates = new ArrayList<>();
+    String audio;
+    LatLng coordinates;
+
+    public Place(PlaceLocate placeLocate, LatLng coordinates, String audio) {
+        locates.add(placeLocate);
+        this.coordinates = coordinates;
+        this.audio = audio;
+    }
+
+    public void addLocate(PlaceLocate placeLocate){
+        locates.add(placeLocate);
+    }
+
+    public String getAudio(Constants.Localization localization){
+        return localization.toString().toLowerCase() + "/" + audio;
+    }
+
+    public String getName(Constants.Localization localization){
+        for (PlaceLocate locate : locates){
+            if (locate.localization == localization){
+                return locate.name;
+            }
+        }
+        return locates.get(0).name;
+    }
+
+    public String getDescription(Constants.Localization localization){
+        for (PlaceLocate locate : locates){
+            if (locate.localization == localization){
+                return locate.description;
+            }
+        }
+        return locates.get(0).description;
+    }
+
+    class PlaceLocate {
+        Constants.Localization localization;
+        String name;
+        String description;
+    }
+}
